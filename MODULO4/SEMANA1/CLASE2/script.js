b165ed05c4da35d8ac0510d52466e7df00b5e88c
EnriqueTC <tishman95@gmail.com>
2024-03-24 16:39:41 -0600

    ,.

---
+++ MODULO4/SEMANA1/CLASE2/script.js
@@ -0,0 +1,22 @@
+const primera = () => {
+    console.log("soy la primera funcion 1")
+}
+
+const segunda = () =>{
+    console.log("soy la segunda funcion 2")
+}
+
+primera()
+segunda()
+
+// Asincronismos: sucesi que no tienen lugar en total correspondencia temporal con otros sucesos.
+
+// Sincronismo: Las tareas se ejecutan de forma secuencial, se debe esperar a que se completen pa continuar con la siguiente tarea. 
+
+// Paralelistmo: Multiples tareas de forma simultanea.
+
+// Concurrencia: Se hace una tarea despues de la otra
+
+// Bloqueante: Una tarea que no devuelve el control jasta que se ha completado.
+
+//No Bloqueante: Una tarea que devuelve inmediatamente cin independencia del resultado.
